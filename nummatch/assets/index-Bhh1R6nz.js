a9b58c9cae982e07c7c1fa4f8f95cc0f1c07da0e
dms-wn615-sys <dms-wn615@hanmail.net>
2026-06-14 12:46:08 +0900

    Deploy number match updates and block game refresh

---
+++ nummatch/assets/index-Bhh1R6nz.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DhjgtXe4.js","assets/index-hdYD8HFX.js","assets/index-D-b5KH4V.css"])))=>i.map(i=>d[i]);
+import{r,_ as t}from"./index-hdYD8HFX.js";const n=r("SplashScreen",{web:()=>t(()=>import("./web-DhjgtXe4.js"),__vite__mapDeps([0,1,2])).then(e=>new e.SplashScreenWeb)});export{n as SplashScreen};
---
+++ nummatch/assets/web-DhjgtXe4.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-hdYD8HFX.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
